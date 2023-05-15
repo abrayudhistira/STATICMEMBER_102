a4f32226b589c7e0f9ae5d050dee06de264e07aa
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-15 14:02:47 +0700

    constructor

---
+++ mbrStatik/destruktor/destruktor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int);     //constructor
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}

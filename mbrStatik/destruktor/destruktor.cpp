f855d0af5e34617c69092337724cfde13b020621
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-15 14:15:46 +0700

    destructor dipanggil

--- mbrStatik/destruktor/destruktor.cpp
+++ mbrStatik/destruktor/destruktor.cpp
@@ -35,5 +35,6 @@ int main()
 {
     angka belajarcpp(3);      //constructor dipanggil
     angka* ptrBelajarcpp = new angka(5);    //constructor dipanggil
+    delete ptrBelajarcpp;               //destructor dipanggil
 
 }
